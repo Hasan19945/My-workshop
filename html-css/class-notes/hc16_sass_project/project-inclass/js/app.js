686165b8414ca75d47cdb225ebcde671d2f63202
hasan <hasancilenger1773@gmail.com>
2023-11-26 22:10:01 +0100

    explore-travel

--- html-css/class-notes/hc16_sass_project/project-inclass/js/app.js
+++ html-css/class-notes/hc16_sass_project/project-inclass/js/app.js
@@ -1,1 +1,9 @@
 //? JS
+//? JS
+
+const hamburgerBtn = document.querySelector(".fa-bars")
+
+hamburgerBtn.addEventListener("click", () => {
+  const headerLinks = document.querySelector(".header__links")
+  headerLinks.classList.toggle("show")
+})
